59b5145d275d4b467a012abf025b397a1e4c0b19
Sara Park <sspark106@gmail.com>
2023-04-17 16:02:54 -0600

    feat: Add test and method to get past trips

--- test/trips-test.js
+++ test/trips-test.js
@@ -122,25 +122,70 @@ describe("Hydration Repository", () => {
         ])
     })
 
-    
+    it('should get past trips', () => {
+        expect(trips.getPastTrips("2022/04/30")).to.deep.equal([{
+            "id": 4,
+            "userID": 43,
+            "destinationID": 14,
+            "travelers": 2,
+            "date": "2022/02/25",
+            "duration": 10,
+            "status": "approved",
+            "suggestedActivities": []
+            }
+        ])
+    })
 
-    it('should return user\'s hydration data when providing an ID number', () => {
-        expect(hydrationRepo.getUserHydrationByID(32)).to.deep.equal([{
-            "userID": 32,
-            "date": "2023/03/30",
-            "numOunces": 62
-         }])
-    });
-  
-    it('should return all of the user\'s hydration data', () => {
-        expect(hydrationRepo.getUserHydrationByID(33).length).to.equal(9);
-    });
-  
-    it('should return an empty array if the given ID number is not an exact match', () => {
-        expect(hydrationRepo.getUserHydrationByID('32')).to.be.empty;
-    });
-  
-    it('should return user\'s all time hydration average', () => {
-        expect(hydrationRepo.userHydrationAllTime(33)).to.equal(62);
-    });
+    it('should get upcoming trips', () => {
+        expect(trips.getUpcomingTrips()).to.deep.equal([{
+            "id": 1,
+            "userID": 44,
+            "destinationID": 49,
+            "travelers": 1,
+            "date": "2022/09/16",
+            "duration": 8,
+            "status": "approved",
+            "suggestedActivities": []
+            },
+            {
+            "id": 2,
+            "userID": 35,
+            "destinationID": 25,
+            "travelers": 5,
+            "date": "2022/10/04",
+            "duration": 18,
+            "status": "approved",
+            "suggestedActivities": []
+            },
+            {
+            "id": 3,
+            "userID": 3,
+            "destinationID": 22,
+            "travelers": 4,
+            "date": "2022/05/22",
+            "duration": 17,
+            "status": "approved",
+            "suggestedActivities": []
+            },
+            {
+            "id": 4,
+            "userID": 43,
+            "destinationID": 14,
+            "travelers": 2,
+            "date": "2022/02/25",
+            "duration": 10,
+            "status": "approved",
+            "suggestedActivities": []
+            },
+            {
+            "id": 5,
+            "userID": 42,
+            "destinationID": 29,
+            "travelers": 3,
+            "date": "2022/04/30",
+            "duration": 18,
+            "status": "approved",
+            "suggestedActivities": []
+        }])
+    })
 })
