0e9f0093c16b08976f555eff6da390c4cb09ceb6
Sara Park <sspark106@gmail.com>
2023-04-17 10:24:19 -0600

    feat: Add dynamic function to fetch data

--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,21 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+window.addEventListener('load', loadHomePage());
 
-console.log('This is the JavaScript entry file - your code begins here.');
+loadHomePage = () => {
+    Promise.all([fetchData('travelers'), fetchData('trips'), fetchData('destinations')])
+  .then(data => {
+    allUsers = new UserRepository(data[0].users);
+    allHydration = new UserHydration(data[1].hydrationData);
+    allSleep = new Sleep(data[2].sleepData);
+    allActivity = new UserActivity(data[3].activityData, data[0].users);
+  })
+}
+
+fetchData = (type) => {
+    return fetch(`http://localhost:3001/api/v1/${type}`)
+    .then(res => res.json())
+    .catch(err => alert('Unable to retrieve data'))
+}
+
