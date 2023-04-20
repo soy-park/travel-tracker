a838b7a0d88d8351bd033090bbe8684d43f16df9
Sara Park <sspark106@gmail.com>
2023-04-20 17:29:54 -0600

    feat: Calculate estimated cost for a chosen destination

--- src/destinations.js
+++ src/destinations.js
@@ -6,6 +6,11 @@ class Destinations {
     getDestinationsByID(destinationId) {
         return this.listOfDestinations.filter((destination) => destination.id === destinationId);
     }
+
+    calculateEstimatedCost(destinationId) {
+        const desiredDest = this.listOfDestinations.filter((destination => destination.id === destinationId));
+        
+    }
 }
 
 export default Destinations;
