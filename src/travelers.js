fd5a25e6fc8b1ab287b1308574576af7dac5d346
Sara Park <sspark106@gmail.com>
2023-04-21 14:09:04 -0600

    feat: Italicize traveler's name

--- src/travelers.js
+++ src/travelers.js
@@ -2,6 +2,11 @@ class Travelers {
     constructor(arrayOfTravelers) {
         this.listOfTravelers = arrayOfTravelers;
     }
+
+    getTravelerNameByID(travelerId) {
+        let travelerInfo = this.listOfTravelers.filter((traveler) => traveler.id === travelerId);
+        return travelerInfo[0].name;
+    }
 }
 
 
